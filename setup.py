eac7e3d0faa5d0c05fdbda5e3200b91de8cabef3
Aadil Hussain <hussainaadil378@gmail.com>
2023-12-15 19:04:03 +0530

    created logger.py and exception.py

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 from setuptools import find_packages,setup
 
+"""HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements"""
+
 setup(
     name='TravelPackagePrediction',
     version='0.0.1',
     author='Aadil Hussain',
     author_email='hussainaadil378@gmail.com',
-    install_requires=["scikit-learn","pandas","numpy"],
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=("pandas","numpy","seaborn","matplotlib","scikit-learn")
 )
