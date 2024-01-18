3aa0e72ea346bc4b04becc01e096be9c48dd50a5
Aadil Hussain <hussainaadil378@gmail.com>
2024-01-18 11:50:18 +0530

    updated workflow and app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def predict_datapoint():
             return render_template('result.html',results=res)
     
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0",port=8080)
 
         
 
